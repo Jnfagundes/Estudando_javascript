44ed7780d00f6a1c317e4f38f30d4e89cc7ea2b5
José Nilton <71674292+Jnfagundes@users.noreply.github.com>
2022-05-17 16:18:58 -0300

    Função

--- exercicio.js
+++ exercicio.js
@@ -1,1 +1,16 @@
+//Declarando uma função
 
+function soma(a, b) { //para declarar a função
+    console.log(a+b) //para apresentar a função
+    return a + b; //para retornar a função
+}
+
+soma(3, 5); //para chamar a função
+
+function returnEvenValues(array) {
+    for(let i = 0; i < array.length;i++){
+        if(array[i] % 2 === 0){
+            
+        }
+    }
+}
