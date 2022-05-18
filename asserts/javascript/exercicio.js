2f012e5c454fe0914b7ca8323c10ca800404d007
José Nilton <71674292+Jnfagundes@users.noreply.github.com>
2022-05-18 11:52:23 -0300

    Criação da index

--- asserts/javascript/exercicio.js
+++ asserts/javascript/exercicio.js
@@ -8,9 +8,13 @@ function soma(a, b) { //para declarar a função
 soma(3, 5); //para chamar a função
 
 function returnEvenValues(array) {
+    let evenNums = [];
     for(let i = 0; i < array.length;i++){
         if(array[i] % 2 === 0){
-            
+            evenNums.push(array[i]);
         }
     }
+    console.log(evenNums);
 }
+
+let array = [1, 2, 4, 5, 7, 8];
