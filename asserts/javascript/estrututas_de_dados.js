f1a1ae47a234da146938cf9395ce70f6ee5d62c2
José Nilton <71674292+Jnfagundes@users.noreply.github.com>
2022-05-24 11:23:14 -0300

    Estudando variável

---
+++ asserts/javascript/estrututas_de_dados.js
@@ -0,0 +1,32 @@
+var a = 1; //Declaração das variáveis
+var b = 2;
+
+if(a === 1){
+    var a = 11;
+    let b = 22; //O let só funciona em escopo de bloco.
+
+    console.log(a); 
+    console.log(b);
+}
+
+//Hoisting é definir o valor da variável antes de declarar ela.
+
+numberOne = 1; //Aqui está sendo feita uma atribuição.
+console.log(numberOne + 2);
+
+var numberOne;
+
+//Quando usar o let, preciso declarar a variável antes da atribuição
+
+var firstName = 'João';
+var lastName = 'Souza';
+
+if(firstName === 'João') {
+    var firstName = 'Pedro';
+    let lastName = 'Silva';
+    lastName = 'Rodrigues'; //Reatribuição do valor da variável
+}
+
+console.log(firstName, lastName);
+
+
