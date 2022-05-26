e98c0f725d9dc93fe2f87cb5eefac65979a2abd7
José Nilton <71674292+Jnfagundes@users.noreply.github.com>
2022-05-26 11:04:01 -0300

    Estudando números

---
+++ asserts/javascript/number.js
@@ -0,0 +1,16 @@
+let num = 10;
+
+let num1 = 200;
+
+let result = num + num1;
+
+num % 2; //módulo para saber se um valor é par
+
+Math.PI //Apresenta o valor de pi
+
+Math.floor(num) //Arredonda para baixo
+
+Math.ceil(num) //Arredonda para cima
+
+percent = 10 
+percent + "%" //Mostra porcentagem
