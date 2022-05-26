b181f1ee3baa937c255440e9ae2c578b56a69bf3
José Nilton <71674292+Jnfagundes@users.noreply.github.com>
2022-05-26 11:26:29 -0300

    Estudando objetos

---
+++ asserts/javascript/Objetos.js
@@ -0,0 +1,15 @@
+let obj = {}; //forma de se inicializar um objeto
+
+obj.name = "Julia"; //criação de uma chave com o valor chamado Julia
+
+obj.age = 20;
+
+Object.values(obj); //usado para saber quais valores estão no objeto
+
+Object.keys(obj); //mostra as chaves desse objeto
+
+let person = {
+    name: "Julia",
+    age: 20,
+    adress: "Rua dois"
+} //declaração de um objeto com os dados
