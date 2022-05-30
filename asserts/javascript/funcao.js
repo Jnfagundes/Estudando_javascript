594b9968ef55bb9755d28f22351820288dbc695e
José Nilton <71674292+Jnfagundes@users.noreply.github.com>
2022-05-30 07:26:28 -0300

    Trabalhando com funções

--- asserts/javascript/funcao.js
+++ asserts/javascript/funcao.js
@@ -29,4 +29,25 @@ const resultSub = calc(sub, 1 ,2);
 console.log(resultSoma);
 console.log(resultSub);
 
+//trabalhando com arrays
+//tecnica Spread
+ function sum(x, y, z) {
+     return x + y + z;
+ }
+
+ const numbers = [1, 2, 3];
+ console.log(sum(...numbers)); //O que era parte de um array se torna um elemento independente.
+
+//tecnica Rest; combina os elementos em um array
+function confereTamanho(...args) { //O que era um elemento independente se torna parte de um array
+    console.log(args.length)
+}
+
+confereTamanho(); //0
+confereTamanho(1, 2); //2
+confereTamanho(3, 4, 5) //3
+
+//Tecnica Object Destructuring
+//Entre chaves podemos filtrar apenas os dados que nos interessam em um objeto
+
 
