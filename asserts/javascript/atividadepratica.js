579d00de3103c9fd03b888c57d0638bb6fa3e167
José Nilton <71674292+Jnfagundes@users.noreply.github.com>
2022-05-30 11:39:49 -0300

    Atividade prática sobre função

---
+++ asserts/javascript/atividadepratica.js
@@ -0,0 +1,33 @@
+//retornar um array com nome de todos os alunos aprovados
+const alunos = [
+{    
+    nome:'João',
+    nota: 5,
+    turma: '18',
+},
+{
+    nome:'Sophia',
+    nota: 9,
+    turma: '18', 
+},
+{
+    nome:'Paulo',
+    nota: 6,
+    turma: '20', 
+}
+];
+function alunosAprovados(arr, media){
+    let aprovados = [];
+    for(let i = 0; i < arr.length; i++){
+        const {nota, nome} = arr[i];
+        if(nota >= media){
+            aprovados.push(nome)
+        }
+    }
+    return aprovados;
+}
+
+console.log(alunosAprovados(alunos, 5));
+
+
+ 
