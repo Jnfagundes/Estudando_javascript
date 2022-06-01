c18f1824390be5ec9ac40ee31cbfc27ab223d27a
José Nilton <71674292+Jnfagundes@users.noreply.github.com>
2022-06-01 10:56:43 -0300

    Atividade

--- asserts/javascript/tratamento_de_erro.js
+++ asserts/javascript/tratamento_de_erro.js
@@ -67,3 +67,31 @@ new Error(message, fileName, lineNumber)
 
 const MeuError = new Error('Mensagem Inválida');
 throw MeuError;
+
+//Atividade
+
+function validaArray(arr, num){
+    try{
+        if(!arr && !num) throw new ReferenceError("Envie os pârametros");
+        if(typeof arr !== 'object') throw new TypeError("O array precisa ser do tipo object");
+        if(typeof num !== 'number') throw new TypeError("O array precisa ser do tipo number");
+
+        if(arr.length !== num) throw new RangeError("Tamanho inválido.");
+    
+        return arr;
+    }
+    catch(e){
+        if(e instanceof ReferenceError) {
+            console.log("Este erro é um ReferenceError!");
+            console.log(e.mensagem);
+        }else if(e instanceof TypeError) {
+            console.log("Este erro é um TypeError!");
+            console.log(e.mensagem);
+        }else if(e instanceof RangeError) {
+            console.log("Este erro é um RangeError!");
+            console.log(e.mensagem);
+        } else {
+            console.log("Tipo de erro não esperado:" + e);
+        }
+    }
+}
