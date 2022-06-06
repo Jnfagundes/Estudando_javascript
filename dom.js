74291b9de9a70d225a3901867919206bbaebfde7
José Nilton <71674292+Jnfagundes@users.noreply.github.com>
2022-06-06 10:58:44 -0300

    Criação da aula dom

---
+++ dom.js
@@ -0,0 +1,3 @@
+//Manipulando a DOM com JavaScript
+//O que DOM? Document Object Model, ou seja, um padrão de como acessar e modificar todos os elementos HTML de uma página
+//O que é bom? Browser Object Model, ou seja, tudo que está dentro do objeto window
