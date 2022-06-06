888b8ba720f21f2c29ed63c82676bbb9fde1305d
José Nilton <71674292+Jnfagundes@users.noreply.github.com>
2022-06-06 15:42:50 -0300

    Atividade Dom

--- dom.js
+++ dom.js
@@ -7,4 +7,18 @@ document.removeChild(element); //Remove um elemento
 document.appendChild(element); //Adiciona um elemento
 document.replaceChild(p, img); //Substitui um elemento. A ordem é (new, old)
 
+// .classList é uma lista de classes
 
+document.getElementsByTagName("p").style.color = "blue";
+
+
+//Eventos: qualquer tipo de ação que o usuário faz em uma página
+//Eventos de mouse: mouseover e mouseout quando posisionamos o mouse em dentro ou fora do item
+//Evento de clique: click e dbclick.
+//Eventos de atualização: Change e load.
+
+const botao = document.getElementById("meuBotao");
+
+botao.addEventListener("click", outraFuncao);
+
+//Atividade prática 
