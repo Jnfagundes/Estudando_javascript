3daae91f287634aebcd9c550d4544991123d324a
José Nilton <71674292+Jnfagundes@users.noreply.github.com>
2022-06-06 15:25:31 -0300

    Dom

--- dom.js
+++ dom.js
@@ -1,3 +1,10 @@
 //Manipulando a DOM com JavaScript
 //O que DOM? Document Object Model, ou seja, um padrão de como acessar e modificar todos os elementos HTML de uma página
 //O que é bom? Browser Object Model, ou seja, tudo que está dentro do objeto window
+
+document.createElement(element); //Cria um novo elemento HTML
+document.removeChild(element); //Remove um elemento
+document.appendChild(element); //Adiciona um elemento
+document.replaceChild(p, img); //Substitui um elemento. A ordem é (new, old)
+
+
